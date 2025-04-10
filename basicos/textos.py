7a18ca874c3b88d77f77ad77dd9cbec734912351
javier <javier@ipartekaula.com>
2025-04-10 21:05:00 +0200

    Textos

---
+++ basicos/textos.py
@@ -0,0 +1,43 @@
+print('Hola\nQué tal estás')
+print('Tenemos el siguiente menú:')
+print('\tUno')
+print('\tDos')
+print('\\')
+
+print('ñlakdjf lñaksdhg lñaksjd fñlkashd gñiuq weopri ñlsdknv ñlxchv' \
+' ñlkasn flknw efih sdljkv ñaseihg lñsdh')
+
+print("Con doble comilla")
+
+print('''
+El menú es el siguiente:
+    Esto
+    Esto otro
+    Y esto más
+      ''')
+
+print('C:\nuevos\trabajos')
+print('C:\\nuevos\\trabajos')
+print(r'C:\nuevos\trabajos')
+
+print(20 * '-')
+
+print('uno y '
+'dos')
+
+nombre = 'Javier'
+
+print('Inicial del nombre', nombre[0])
+
+print('Letra final del nombre', nombre[-1])
+
+print('Abreviatura de Javier', nombre[0:4])
+
+print('Extensión de video', nombre[1:4])
+
+abreviatura = nombre[:4]
+
+print('Abreviatura de Javier', abreviatura)
+
+print('Nombre completo', abreviatura + nombre[4:])
+
