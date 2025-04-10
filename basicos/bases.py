0fad1fef51f6724c5759973511f5e5c8cd234ac5
javier <javier@ipartekaula.com>
2025-04-10 18:54:04 +0200

    Ejemplo básico inicial

---
+++ basicos/bases.py
@@ -0,0 +1,20 @@
+# nombre = input('Dime tu nombre: ') # 'Javier Lete'
+
+# print('Hola', nombre) # Muestra Hola mundo en pantalla
+
+# saludo = 'Hola ' + nombre
+
+# print(saludo)
+
+# saludo = 42
+
+# print(saludo)
+
+x = float(input('Dime x: ')) # Pedimos la x y la convertimos a entero
+y = float(input('Dime y: '))
+
+# print(type(x), type(y))
+
+suma = x + y
+
+print(suma)
