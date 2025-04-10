fde7cc84e83ff838402df83e7f296c6cb5759c52
javier <javier@ipartekaula.com>
2025-04-10 21:04:25 +0200

    Ejemplo de imports

---
+++ basicos/import.py
@@ -0,0 +1,27 @@
+numero = 2.5
+
+print(round(numero))
+
+import math
+
+print('La raíz cuadrada de 4 es', math.sqrt(4.0))
+
+print(math.floor(numero + 0.5))
+
+import math as mates
+
+print('La raíz cuadrada de 4 es', mates.sqrt(4.0))
+
+print(mates.floor(numero + 0.5))
+
+from math import *
+
+print('La raíz cuadrada de 4 es', sqrt(4.0))
+
+print(floor(numero + 0.5))
+
+print('El número es', numero)
+
+del numero # No es muy habitual
+
+print('El número es', numero)
