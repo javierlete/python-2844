0e0afa7c7a04a4be5b5559a295841895a85bfe44
javier <javier@ipartekaula.com>
2025-04-14 20:58:35 +0200

    Repetitivas

---
+++ basicos/repetitivas.py
@@ -0,0 +1,15 @@
+contador = 1
+
+while contador <= 10:
+    print(contador, end=' ')
+    contador = contador + 1
+
+print()
+
+for dato in range(1, 11):
+    print(dato, end=' ')
+
+print()
+
+for dato in range(10, 0, -1):
+    print(dato, end=' ')
