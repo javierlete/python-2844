16ca9b9149e56d54a2935c9294abac57bb851ce3
javier <javier@ipartekaula.com>
2025-04-22 20:58:56 +0200

    Uso de else en repetitivas

--- basicos/repetitivas.py
+++ basicos/repetitivas.py
@@ -13,3 +13,19 @@ print()
 
 for dato in range(10, 0, -1):
     print(dato, end=' ')
+
+lista = [1, 5, 78, 3, 5]
+
+print()
+
+for dato in lista:
+    match dato:
+        case 4:
+            print('Encontrado 4')
+            break
+        case 6:
+            print('Encontrado 6')
+            break
+else:
+    print('NO encontrado')
+
