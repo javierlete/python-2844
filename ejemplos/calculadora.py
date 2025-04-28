8fe9c3311ae45b329824d5e7a778a8e6eb63f793
javier <javier@ipartekaula.com>
2025-04-28 20:50:15 +0200

    Convertimos calculadora en una función con llamada desde la consola

--- ejemplos/calculadora.py
+++ ejemplos/calculadora.py
@@ -1,14 +1,15 @@
-a = float(input('Dime el primer operando: '))
-op = input('Dime la operación: ')
+def calcular(a, op, b):
+    match op:
+        case '+':
+            operacion = lambda x, y: x + y
+        case '-':
+            operacion = lambda x, y: x - y
+        case _:
+            raise ValueError('No se esperaba esa operacion')
 
-match op:
-    case '+':
-        operacion = lambda x, y: x + y
-    case '-':
-        operacion = lambda x, y: x - y
-    case _:
-        raise ValueError('No se esperaba esa operacion')
+    return operacion(a, b)
 
-b = float(input('Dime el segundo operando: '))
-
-print(operacion(a, b))
+# Comprobación de ejecución directa desde python.exe
+if __name__ == "__main__":
+    import sys
+    print(calcular(float(sys.argv[1]), sys.argv[2], float(sys.argv[3])))
